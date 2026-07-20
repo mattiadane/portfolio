017ae5dda66dd9f73af6e1bf190a662f57e19662
mattiadane <mattia.danese04@gmail.com>
2026-07-20 16:05:00 +0200

    Corretto un bug

--- src/components/UbuntuDock.jsx
+++ src/components/UbuntuDock.jsx
@@ -14,10 +14,10 @@ function UbuntuDock() {
     <div className="fixed left-0 top-0 h-screen w-20 bg-neutral-950 flex flex-col items-center py-8 gap-8 shadow-xl z-40">
       {/* HOME */}
       <div className="relative flex flex-col items-center">
-        <NavLink to="/" className="text-5xl text-white hover:opacity-80">
+        <NavLink to="/portfolio/" className="text-5xl text-white hover:opacity-80">
           🏠
         </NavLink>
-        {isActive("/") && (
+        {isActive("/portfolio/") && (
           <div className="absolute left-0 top-1/2 -translate-y-1/2 w-2 h-2 bg-orange-500 rounded-full"></div>
         )}
       </div>
